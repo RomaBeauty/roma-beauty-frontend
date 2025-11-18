a0106cd8d5b369a0b06b5bc9ec0c3449a834f774
gustavomurar <gustavomurarosoares3@gmail.com>
2025-11-18 16:41:58 -0300

    feat: PWA icons.

--- vite.config.js
+++ vite.config.js
@@ -35,18 +35,19 @@ export default defineConfig({
       
         icons: [
           {
-            src: 'icons/icon-192.png',
+            src: '/icons/icon-192.png',
             sizes: '192x192',
             type: 'image/png',
             purpose: 'any'
           },
           {
-            src: 'icons/icon-512.png',
+            src: '/icons/icon-512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'maskable any'
           }
         ]
+        
       },
       devOptions: {
         enabled: true
